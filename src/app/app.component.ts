9314ded39ba5755c5def02b11bd20ba0916a2b2f
Lakshay <ihybrid11@gmail.com>
2019-03-23 17:22:35 +0530

    MAP API is working. Able to fetch locations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,9 @@ export class AppComponent implements OnInit {
   lat = 51.678418;
   lng = 7.809007;
 
+  results;
+  locationFound = false;
+
   private latitude = 0.0;
   private longitude = 0.0;
 
@@ -32,8 +35,12 @@ export class AppComponent implements OnInit {
 
   fetchLocation() {
     navigator.geolocation.getCurrentPosition((position) => {
-      console.log('lat== ' + position.coords.latitude);
-      console.log('lng== ' + position.coords.longitude);
+      this.locationFound =  true;
+      this.latitude = position.coords.latitude;
+      this.longitude = position.coords.longitude;
+
+      this.fetchPlaces();
+
     }, () => {
       alert('location not found');
     }, {enableHighAccuracy: true});
@@ -44,13 +51,19 @@ export class AppComponent implements OnInit {
     console.log(pinCode);
 
     const geocoder = new google.maps.Geocoder();
+    // tslint:disable-next-line:object-literal-key-quotes
     geocoder.geocode({ 'address': pinCode}, (results, status) => {
+      console.log(status);
 
       if (status === google.maps.GeocoderStatus.OK) {
+        this.locationFound = true;
         this.latitude = results[0].geometry.location.lat();
         this.longitude = results[0].geometry.location.lng();
+
+        this.fetchPlaces();
       } else {
-          alert('Geocode was not successful for the following reason: ' + status);
+          // tslint:disable-next-line:comment-format
+          //alert('Geocode was not successful for the following reason: ' + status);
       }
   });
   }
@@ -58,13 +71,14 @@ export class AppComponent implements OnInit {
   fetchPlaces() {
     console.log('lat== ' + this.latitude);
     console.log('lng== ' + this.longitude);
-    let service = new google.maps.places.PlacesService(this.gmapElement.nativeElement);
+    const service = new google.maps.places.PlacesService(this.gmapElement.nativeElement);
     service.nearbySearch({
       location: {lat: this.latitude, lng: this.longitude},
       radius: 5000,
       type: 'restaurant'
     }, (results, status) => {
-      console.log(results);
+      this.results = results;
+      this.locationFound = false;
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AgmCoreModule } from '@agm/core';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { AppComponent } from './app.component';
 
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CommonModule,
     FormsModule,
+    AngularFontAwesomeModule,
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyCHY86W7jVXHyVRUgwTpV0ZHjbQCTJSPGw'
     })
