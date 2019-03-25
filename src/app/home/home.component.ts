dba1dc2f86ec3545fd7054c774d9474776aa4c80
lakshay <ihybrid11@gmail.com>
2019-03-26 00:15:31 +0530

    refactor complete

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AppService } from '../shared/app.service';
 
 @Component({
   selector: 'app-home',
@@ -19,66 +20,26 @@ export class HomeComponent implements OnInit {
   private latitude = 0.0;
   private longitude = 0.0;
 
+  constructor (private appService : AppService) {}
 
   @ViewChild('gmap') gmapElement: any;
   map: google.maps.Map;
 
   ngOnInit() {
-    const mapProp = {
-      center: new google.maps.LatLng(28.5879199, 77.0620314),
-      zoom: 16
-    };
+    const mapProp = this.appService.getMapProp();
 
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
   }
 
   fetchLocation() {
-    navigator.geolocation.getCurrentPosition((position) => {
-      this.locationFound = true;
-      this.latitude = position.coords.latitude;
-      this.longitude = position.coords.longitude;
-
-      this.fetchPlaces();
-
-    }, () => {
-      alert('location not found');
-    }, { enableHighAccuracy: true });
+    this.results = this.appService.fetchPlacesWithAutoDetectLocation(this.gmapElement);
   }
 
   fetchLocationByPin(form: NgForm) {
     const pinCode = form.value.pinCode;
     this.placeType = form.value.type;
 
-    const geocoder = new google.maps.Geocoder();
-    // tslint:disable-next-line:object-literal-key-quotes
-    geocoder.geocode({ 'address': pinCode }, (results, status) => {
-      //console.log(status);
-
-      if (status === google.maps.GeocoderStatus.OK) {
-        this.locationFound = true;
-        this.latitude = results[0].geometry.location.lat();
-        this.longitude = results[0].geometry.location.lng();
-
-      } else {
-        // tslint:disable-next-line:comment-format
-        //alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-    this.fetchPlaces();
-  }
-
-  fetchPlaces() {
-    console.log('lat== ' + this.latitude);
-    console.log('lng== ' + this.longitude);
-    const service = new google.maps.places.PlacesService(this.gmapElement.nativeElement);
-    service.nearbySearch({
-      location: { lat: this.latitude, lng: this.longitude },
-      radius: 5000,
-      type: this.placeType
-    }, (results, status) => {
-      this.results = results;
-      this.locationFound = false;
-    });
+    this.results = this.appService.fetchPlacesWithPinCode(pinCode,this.gmapElement);
   }
 
 }
---
+++ src/app/shared/map.service.ts
@@ -0,0 +1,59 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class MapServices {
+    constructor() { }
+
+    results;
+    locationFound = false;
+    placeType: string = 'restaurant';
+
+    private latitude = 0.0;
+    private longitude = 0.0;
+    map: google.maps.Map;
+
+    getPlacesResults () {
+        return this.results;
+    }
+
+    autoDetectLocation(gmapElement) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            this.locationFound = true;
+            this.latitude = position.coords.latitude;
+            this.longitude = position.coords.longitude;
+            this.fetchPlaces(gmapElement);
+        }, () => {
+            alert('location not found');
+        }, { enableHighAccuracy: true });
+    }
+
+    detectLocationByPin(pincode, gmapElement) {
+        const geocoder = new google.maps.Geocoder();
+        geocoder.geocode({ 'address': pincode }, (results, status) => {
+            if (status === google.maps.GeocoderStatus.OK) {
+                this.locationFound = true;
+                this.latitude = results[0].geometry.location.lat();
+                this.longitude = results[0].geometry.location.lng();
+                this.fetchPlaces(gmapElement);
+            } else {
+
+            }
+        })
+    }
+
+    fetchPlaces(gmapElement) {
+        // console.log('lat== ' + this.latitude);
+        // console.log('lng== ' + this.longitude);
+        const service = new google.maps.places.PlacesService(gmapElement.nativeElement);
+        service.nearbySearch({
+            location: { lat: this.latitude, lng: this.longitude },
+            radius: 5000,
+            type: this.placeType
+        }, (results, status) => {
+            this.results = results;
+            this.locationFound = false;
+        });
+    }
+}
