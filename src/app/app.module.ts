1ac6a5338625aa134ec55ae090ecdb2951205a2c
Lakshay <ihybrid11@gmail.com>
2019-06-02 08:42:08 +0530

    fixed security/vulnurabilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,15 +14,16 @@ import { UserComponent } from './usercomponent/user.component';
 import { LoginComponent } from './login/login.component';
 import { routing } from './shared/app.routing';
 import { HttpService } from './shared/http.service';
+import keys from './config/keys.js';
 
 const config = new AuthServiceConfig([
   {
     id: GoogleLoginProvider.PROVIDER_ID,
-    provider: new GoogleLoginProvider("943250029374-nr1h2d4q9jeldce3f8h36dg39b2vl7da.apps.googleusercontent.com")
+    provider: new GoogleLoginProvider(keys.google.clientID)
   },
   {
     id: FacebookLoginProvider.PROVIDER_ID,
-    provider: new FacebookLoginProvider("718547185265921")
+    provider: new FacebookLoginProvider(keys.google.facebook)
   }
 ]);
 
@@ -44,7 +45,7 @@ export function provideConfig() {
     FormsModule,
     AngularFontAwesomeModule,
     AgmCoreModule.forRoot({
-      apiKey: 'AIzaSyCHY86W7jVXHyVRUgwTpV0ZHjbQCTJSPGw'
+      apiKey: keys.google.gmap
     }),
     routing,
     SocialLoginModule
